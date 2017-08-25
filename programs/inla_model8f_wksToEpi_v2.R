6f3520c37ea3f747d6c9b6d28bef05a5b69776db
Elizabeth Lee <eclee25@gmail.com>
2017-08-25 15:24:49 -0400

    8f V2-11, nbinom lik
    inla_model8f_wksToEpi_v2.R
    - 8f V2-11, change from poisson to negative binomial likelihood
    because the response data may be over-dispersed, causing poorer
    model fit with the poiss likelihood

--- programs/inla_model8f_wksToEpi_v2.R
+++ programs/inla_model8f_wksToEpi_v2.R
@@ -20,9 +20,9 @@ require(RColorBrewer); require(ggplot2) # export_inlaData_st dependencies
 
 #### set these! ################################
 dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
-modCodeStr <- "8f_wksToEpi_v2-8_rmSeasFx"
+modCodeStr <- "8f_wksToEpi_v2-11"
 rdmFx_RV <- "phi"
-likString <- "poisson"
+likString <- "nbinomial"
 origin_locations_file <- "Lee"
 dig <- 4 # number of digits in the number of elements at this spatial scale (~3000 counties -> 4 digits)
 s <- 999 # all seasons code for spatiotemporal analysis = 999
@@ -80,7 +80,7 @@ formula <- Y ~ -1 +
   f(graphIdx_nonzero, model = "besag", graph = path_adjMxExport_cty) +
   f(fips_st_nonzero, model = "iid") +
   f(regionID_nonzero, model = "iid") +
-  # f(season_nonzero, model = "iid") +
+  f(season_nonzero, model = "iid") +
   intercept_nonzero + O_imscoverage_nonzero + O_careseek_nonzero + O_insured_nonzero + X_poverty_nonzero + X_child_nonzero + X_adult_nonzero + X_hospaccess_nonzero + X_popdensity_nonzero + X_housdensity_nonzero + X_vaxcovI_nonzero + X_vaxcovE_nonzero + X_H3A_nonzero + X_B_nonzero + X_priorImmunity_nonzero + X_anomHumidity_nonzero + X_pollution_nonzero + X_singlePersonHH_nonzero + X_latitude_nonzero
 
   # + X_sourceLocDist_nonzero + X_humidity_nonzero
@@ -103,7 +103,7 @@ path_csvExport <- getwd()
 modData_hurdle <- convert_hurdleModel_nz_spatiotemporal(modData_full)
 
 mod <- inla(formula,
-            family = "poisson",
+            family = "nbinomial",
             data = modData_hurdle,
             control.fixed = list(mean = 0, prec = 1/100), # set prior parameters for regression coefficients
             control.predictor = list(compute = TRUE, link = rep(1, nrow(modData_full))),
