00c2e3e3792a19cccb918c25d3dcf12fbe244fae
Elizabeth Lee <eclee25@gmail.com>
2017-10-21 02:05:29 -0400

    redefine early flu season exponential period
    exponential growth should have r greater than 1 and the same rate
    of r
    
    reference_earlySeasonDefinition.R
    - redefine exponential growth period to identify consecutive
    periods with limited variance in r

--- programs/reference_earlySeasonDefinition.R
+++ programs/reference_earlySeasonDefinition.R
@@ -9,11 +9,11 @@
 ## install.packages("pkg", dependencies=TRUE, lib="/usr/local/lib/R/site-library") # in sudo R
 ## update.packages(lib.loc = "/usr/local/lib/R/site-library")
 ####################################
-require(tidyverse)
+require(tidyverse); require(RcppRoll)
 setwd(dirname(sys.frame(1)$ofile))
 setwd("../R_export")
 
-fullDat <- read_csv("fullIndicAll_periodicReg_ilinDt_Octfit_span0.4_degree2_analyzeDB_nat.csv")
+fullDat <- read_csv("fullIndicAll_periodicReg_irDt_Octfit_span0.4_degree2_analyzeDB_nat.csv")
 
 setwd("../graph_outputs")
 exportPath <- paste0(getwd(),"/reference_earlySeasonDefinition/")
@@ -26,8 +26,8 @@ consider.expGrowth_maxconsec <- function(x){
     dummy <- rep(F, length(x))
     pre.index <- ((which(rle.results$values & rle.results$lengths==max.consec))[1])-1 # rle index of value prior to first value in longest consecutive run of TRUE
     post.index <- tail((which(rle.results$values & rle.results$lengths==max.consec)), n=1) # rle index of last value in longest consec run of TRUE 
-    converted.pre.index <- ifelse(pre.index==1, 0, sum(rle.results$lengths[1:(pre.index-1)])) # vector index - 1 of first value in longest consec run of TRUE
-    converted.post.index <- ifelse(post.index, ifelse(sum(rle.results$lengths[1:post.index]) <= converted.pre.index+3, sum(rle.results$lengths[1:post.index]), converted.pre.index+3), NA) # vector index of last value in longest consec run of TRUE, with max period of 3 weeks
+    converted.pre.index <- ifelse(pre.index==1, 0, sum(rle.results$lengths[1:(pre.index)])-1) # vector index - 1 of first value in longest consec run of TRUE
+    converted.post.index <- ifelse(post.index, ifelse(sum(rle.results$lengths[1:post.index]) <= converted.pre.index+3, sum(rle.results$lengths[1:post.index]), converted.pre.index+2), NA) # vector index of last value in longest consec run of TRUE, with max period of 3 weeks if observations meet CV criteria; max period of 2 weeks if observations achieve only minimum rolling CV
     if(!is.na(converted.pre.index)){
       dummy[(converted.pre.index+1):converted.post.index] <- T
     }
@@ -53,49 +53,61 @@ inEarlySeasonDat <- left_join(fullDat, processDat, by = c("season")) %>%
     select(season, Thu.week, in.earlyseason)
 
 #### dynamically based on exponential growth ################################
-# r must be positive and the period may endure only 2-3 weeks
+# r must be positive, with small variance, and the period may endure only 2-3 weeks
 
 n0Dat <- fullDat %>%
   filter(season > 2) %>%
   mutate(t = t+1) %>%
-  rename(N0 = ilin.dt) %>%
+  rename(N0 = ir.dt) %>%
   select(t, N0)
 
 processDat2 <- full_join(fullDat, n0Dat, by = c("t")) %>%
   filter(season > 2 & in.season) %>%
   group_by(season) %>%
-  mutate(tsteps = seq_along(ilin.dt)-1) %>%
+  mutate(tsteps = seq_along(ir.dt)-1) %>%
   ungroup %>%
-  mutate(expRate = ifelse(tsteps>0, log(ilin.dt/N0), NA)) %>%
-  select(Thu.week, flu.week, ilin.dt, season, epi.thresh, epi.week, has.epi, in.season, N0, tsteps, expRate) %>%
-  mutate(expGrowth = ifelse(expRate > 0, TRUE, FALSE)) %>%
+  mutate(expRate = ifelse(tsteps>0, log(ir.dt/N0), NA)) %>%
+  select(Thu.week, flu.week, ir.dt, season, epi.thresh, epi.week, has.epi, in.season, N0, tsteps, expRate) %>%
+  mutate(posR = ifelse(expRate > 0, TRUE, FALSE)) %>%
+  mutate(rollingCV = roll_sd(expRate, n=3, fill=NA)/expRate) 
+
+# identify minimum rolling coefficient of variation for each season
+minRollingDat <- processDat2 %>%
+  filter(posR) %>%
+  group_by(season) %>%
+  mutate(minRollingCV = min(rollingCV, na.rm = TRUE)) %>%
+  ungroup %>%
+  distinct(season, minRollingCV)
+
+# early season includes week before expGrowth = TRUE
+processDat3 <- full_join(processDat2, minRollingDat, by = c("season")) %>%
+  mutate(expGrowth = ifelse((posR & (rollingCV < .25)), TRUE, ifelse(posR & (rollingCV < (1.1*minRollingCV)), TRUE, FALSE))) %>%
   group_by(season) %>%
   mutate(in.earlyseason = consider.expGrowth_maxconsec(expGrowth)) %>%
-  ungroup
+  ungroup 
 
-inEarlySeasonDat2 <- processDat2 %>%
+inEarlySeasonDat2 <- processDat3 %>%
    filter(in.earlyseason) %>%
    select(season, Thu.week, in.earlyseason)
 
 #### export reference datasets ################################
 
 setwd("../reference_data")
-write_csv(inEarlySeasonDat, "earlySeasonDefinition_4wks.csv")
-write_csv(inEarlySeasonDat2, "earlySeasonDefinition_expRate.csv")
+write_csv(inEarlySeasonDat2, "earlySeasonDefinition_irDt_expRate.csv")
 
 #### export plots ################################
 
-iliplts <- ggplot(processDat2, aes(x = tsteps, y = ilin.dt, group = 1)) +
+iliplts <- ggplot(processDat3, aes(x = tsteps, y = ir.dt, group = 1)) +
     geom_line(aes(colour = in.earlyseason)) +
     facet_wrap(~season) +
     theme_bw() + 
     theme(text = element_text(size = 12))
-    ggsave(paste0(exportPath, "ilinDt_nat_bySeason.png"), iliplts, height = h, width = w, dpi = dp)
+    ggsave(paste0(exportPath, "irDt_nat_bySeason.png"), iliplts, height = h, width = w, dpi = dp)
 
-rplts <- ggplot(processDat2, aes(x = tsteps, y = expRate, group = 1)) +
+rplts <- ggplot(processDat3, aes(x = tsteps, y = expRate, group = 1)) +
     geom_line(aes(colour = in.earlyseason)) +
     geom_hline(yintercept = 0) +
     facet_wrap(~season) +
     theme_bw() + 
     theme(text = element_text(size = 12))
-    ggsave(paste0(exportPath, "r_nat_bySeason.png"), rplts, height = h, width = w, dpi = dp)
+    ggsave(paste0(exportPath, "r_irDt_nat_bySeason.png"), rplts, height = h, width = w, dpi = dp)
