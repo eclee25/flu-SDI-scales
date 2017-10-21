179b1a1e1b3d7fba0eb21ffc4a203e24aa5a2fcf
Elizabeth Lee <eclee25@gmail.com>
2017-10-21 02:10:05 -0400

    explore choropleths for IR response data
    main_response_data_explore.R
    - choropleths for ersponse data for cty, st, and region levels

--- programs/main_response_data_explore.R
+++ programs/main_response_data_explore.R
@@ -8,35 +8,63 @@ setwd(dirname(sys.frame(1)$ofile))
 source("source_import_modeldata.R")
 ####################################
 
-dbCodeStr <- "_ilinDt_Octfit_span0.4_degree2"
+dbCodeStr <- "_irDt_Octfit_span0.4_degree2"
 w <- 5.5; h = 4
 
 setwd('../reference_data')
 path_abbr_st <- paste0(getwd(), "/state_abbreviations_FIPS.csv")
 path_latlon_cty <- paste0(getwd(), "/cty_pop_latlon.csv")
+path_latlon_st <- paste0(getwd(), "/state_latlon.csv")
+path_latlon_reg <- paste0(getwd(), "/region_latlon.csv")
+path_region_cw <- paste0(getwd(), "/state_abbreviations_FIPS_region.csv")
 
 setwd("../R_export")
 path_response_cty <- paste0(getwd(), sprintf("/dbMetrics_periodicReg%s_analyzeDB_cty.csv", dbCodeStr))
 path_fullIndic_cty <- paste0(getwd(), sprintf("/fullIndicAll_periodicReg%s_analyzeDB_cty.csv", dbCodeStr))
+path_response_st <- paste0(getwd(), sprintf("/dbMetrics_periodicReg%s_analyzeDB_st.csv", dbCodeStr))
+path_fullIndic_st <- paste0(getwd(), sprintf("/fullIndicAll_periodicReg%s_analyzeDB_st.csv", dbCodeStr))
+path_response_reg <- paste0(getwd(), sprintf("/dbMetrics_periodicReg%s_analyzeDB_reg.csv", dbCodeStr))
+path_fullIndic_reg <- paste0(getwd(), sprintf("/fullIndicAll_periodicReg%s_analyzeDB_reg.csv", dbCodeStr))
+
 
 path_list <- list(path_abbr_st = path_abbr_st,
                   path_latlon_cty = path_latlon_cty,
+                  path_region_cw = path_region_cw,
                   path_response_cty = path_response_cty,
-                  path_fullIndic_cty = path_fullIndic_cty)
+                  path_fullIndic_cty = path_fullIndic_cty, 
+                  path_latlon_st = path_latlon_st,
+                  path_response_st = path_response_st,
+                  path_fullIndic_st = path_fullIndic_st, 
+                  path_latlon_reg = path_latlon_reg,
+                  path_response_reg = path_response_reg,
+                  path_fullIndic_reg = path_fullIndic_reg)
+offset <- FALSE
 
 setwd("../graph_outputs/response_data_explore")
 exportPath <- getwd()
 
-wksToEpi <- import_obs_wksToEpi(path_list)
-wksToPeak <- import_obs_wksToPeak(path_list)
-iliEarly <- import_obs_iliEarly(path_list)
-iliPeak <- import_obs_iliPeak(path_list)
+# import cty & st data
+wksToEpi_ctySt <- import_obs_wksToEpi_ctySt(offset, path_list)
+wksToPeak_ctySt <- import_obs_wksToPeak_ctySt(offset, path_list)
+iliEarly_ctySt <- import_obs_iliEarly_ctySt(offset, path_list)
+iliPeak_ctySt <- import_obs_iliPeak_ctySt(offset, path_list)
+# import cty & reg data (so it's plottable on a cty map)
+wksToEpi_ctyReg <- import_obs_wksToEpi_ctyReg(offset, path_list)
+wksToPeak_ctyReg <- import_obs_wksToPeak_ctyReg(offset, path_list)
+iliEarly_ctyReg <- import_obs_iliEarly_ctyReg(offset, path_list)
+iliPeak_ctyReg <- import_obs_iliPeak_ctyReg(offset, path_list)
+
+# merge data for all three scales
+wksToEpi <- merge_obs_ctyStReg(wksToEpi_ctySt, wksToEpi_ctyReg)
+wksToPeak <- merge_obs_ctyStReg(wksToPeak_ctySt, wksToPeak_ctyReg)
+iliEarly <- merge_obs_ctyStReg(iliEarly_ctySt, iliEarly_ctyReg)
+iliPeak <- merge_obs_ctyStReg(iliPeak_ctySt, iliPeak_ctyReg)
+
 
 ################################
-choro_obs_db_oneSeason <- function(inDat, pltFormats){
+choro_obs_db_oneSeason <- function(prepDat, pltFormats){
   # plot single season choropleth for disease burden measure
-  print(match.call())
-  
+
   # plot formatting
   w <- pltFormats$w; h <- pltFormats$h; dp <- 300
   if (is.null(pltFormats$legendStep)){
@@ -46,9 +74,10 @@ choro_obs_db_oneSeason <- function(inDat, pltFormats){
   }
   offset_l <- pltFormats$offset_l
   measure <- pltFormats$measure
+  dataProcess <- pltFormats$dataProcess
+  dataScale <- pltFormats$dataScale
 
-  prepDat <- inDat %>%
-    mutate(obs_rr = obs_y/E)
+  names(prepDat) <- gsub(paste0("y_", dataScale), "y", names(prepDat)) 
 
   # set breaks based on distribution of observed data
   if(!offset_l){
@@ -74,7 +103,7 @@ choro_obs_db_oneSeason <- function(inDat, pltFormats){
 
   
   plotChoro <- function(x){
-    exportFname <- paste0(exportPath, "/choro_obs_", measure, "_S", x, ".png")
+    exportFname <- paste0(exportPath, "/choro_obs_", dataProcess, "_", measure, "_", dataScale, "_S", x, ".png")
     pltDat <- plotDat %>% filter(season == x)
 
     # import county mapping info
@@ -98,7 +127,6 @@ choro_obs_db_oneSeason <- function(inDat, pltFormats){
 ################################
 choro_obs_db_avgSeason <- function(inDat, pltFormats){
   # plot choropleth across seasons for disease burden measure
-  print(match.call())
   
   # plot formatting
   w <- pltFormats$w; h <- pltFormats$h; dp <- 300
@@ -109,6 +137,10 @@ choro_obs_db_avgSeason <- function(inDat, pltFormats){
   }
   offset_l <- pltFormats$offset_l
   measure <- pltFormats$measure
+  dataProcess <- pltFormats$dataProcess
+  dataScale <- pltFormats$dataScale
+
+  names(inDat) <- gsub(paste0("y_", dataScale), "y", names(inDat)) 
 
   prepDat <- inDat %>%
     group_by(fips) %>%
@@ -125,17 +157,16 @@ choro_obs_db_avgSeason <- function(inDat, pltFormats){
     prepDat2 <- prepDat %>%
       mutate(Observed = cut(obs_rr, breaks, right = TRUE, include.lowest = TRUE, ordered_result = TRUE)) 
   }
-  
   factorlvls <- levels(prepDat2$Observed)
-  plotDat <- prepDat2 %>%
-    select(season, fips, Observed) %>%
+  pltDat <- prepDat2 %>%
+    select(fips, Observed) %>%
     gather(fig, bin, Observed) %>%
     filter(fig == "Observed") %>%
     mutate(fig = factor(fig, levels = c("Observed"))) %>%
     mutate(bin = factor(bin, levels = factorlvls, labels = factorlvls, ordered = TRUE)) 
-  print(levels(plotDat$bin))
+  print(levels(pltDat$bin))
  
-  exportFname <- paste0(exportPath, "/choro_obs_", measure, "_avg.png")
+  exportFname <- paste0(exportPath, "/choro_obs_", dataProcess, "_", measure, "_", dataScale, "_avg.png")
 
   # import county mapping info
   ctyMap <- import_county_geomMap()
@@ -155,16 +186,53 @@ choro_obs_db_avgSeason <- function(inDat, pltFormats){
 
 #### MAIN CODE ################################
 
-# figures by season
-format_wksToEpi <- list(w = w, h = h, measure = "wksToEpi", legendStep = 6, offset_l = FALSE)
-do.call(choro_obs_db_oneSeason, c(wksToEpi, format_wksToEpi))
-do.call(choro_obs_db_avgSeason, c(wksToEpi, format_wksToEpi))
+## county data figures ##
+format_wksToEpi <- list(w = w, h = h, measure = "wksToEpi", dataProcess = "irDt", legendStep = 5, offset_l = FALSE, dataScale = "cty")
+do.call(choro_obs_db_oneSeason, list(wksToEpi, format_wksToEpi))
+do.call(choro_obs_db_avgSeason, list(wksToEpi, format_wksToEpi))
 
-# format_wksToPeak <- list(w = w, h = h, measure = "wksToPeak", legendStep = 6, offset_l = FALSE)
-# choro_obs_db_oneSeason(wksToPeak, format_wksToPeak)
+format_wksToPeak <- list(w = w, h = h, measure = "wksToPeak", dataProcess = "irDt", legendStep = 4, offset_l = FALSE, dataScale = "cty")
+do.call(choro_obs_db_oneSeason, list(wksToPeak, format_wksToPeak))
+do.call(choro_obs_db_avgSeason, list(wksToPeak, format_wksToPeak))
 
-# format_iliEarly <- list(w = w, h = h, measure = "iliEarly", legendStep = 10, offset_l = FALSE)
-# choro_obs_db_oneSeason(iliEarly, format_iliEarly)
+format_iliEarly <- list(w = w, h = h, measure = "iliEarly", dataProcess = "irDt", legendStep = 0.5, offset_l = FALSE, dataScale = "cty")
+do.call(choro_obs_db_oneSeason, list(iliEarly, format_iliEarly))
+do.call(choro_obs_db_avgSeason, list(iliEarly, format_iliEarly))
 
-# format_iliPeak <- list(w = w, h = h, measure = "iliPeak", legendStep = 9, offset_l = FALSE)
-# choro_obs_db_oneSeason(iliPeak, format_iliPeak)
+format_iliPeak <- list(w = w, h = h, measure = "iliPeak", dataProcess = "irDt", legendStep = 0.5, offset_l = FALSE, dataScale = "cty")
+do.call(choro_obs_db_oneSEason, list(iliPeak, format_iliPeak))
+do.call(choro_obs_db_avgSeason, list(iliPeak, format_iliPeak))
+
+## state data figures ##
+format_wksToEpi <- list(w = w, h = h, measure = "wksToEpi", dataProcess = "irDt", legendStep = 5, offset_l = FALSE, dataScale = "st")
+do.call(choro_obs_db_oneSeason, list(wksToEpi, format_wksToEpi))
+do.call(choro_obs_db_avgSeason, list(wksToEpi, format_wksToEpi))
+
+format_wksToPeak <- list(w = w, h = h, measure = "wksToPeak", dataProcess = "irDt", legendStep = 4, offset_l = FALSE, dataScale = "st")
+do.call(choro_obs_db_oneSeason, list(wksToPeak, format_wksToPeak))
+do.call(choro_obs_db_avgSeason, list(wksToPeak, format_wksToPeak))
+
+format_iliEarly <- list(w = w, h = h, measure = "iliEarly", dataProcess = "irDt", legendStep = 50, offset_l = FALSE, dataScale = "st")
+do.call(choro_obs_db_oneSeason, list(iliEarly, format_iliEarly))
+do.call(choro_obs_db_avgSeason, list(iliEarly, format_iliEarly))
+
+format_iliPeak <- list(w = w, h = h, measure = "iliPeak", dataProcess = "irDt", legendStep = 50, offset_l = FALSE, dataScale = "st")
+do.call(choro_obs_db_oneSeason, list(iliPeak, format_iliPeak))
+do.call(choro_obs_db_avgSeason, list(iliPeak, format_iliPeak))
+
+## region data figures ##
+format_wksToEpi <- list(w = w, h = h, measure = "wksToEpi", dataProcess = "irDt", legendStep = 5, offset_l = FALSE, dataScale = "reg")
+do.call(choro_obs_db_oneSeason, list(wksToEpi, format_wksToEpi))
+do.call(choro_obs_db_avgSeason, list(wksToEpi, format_wksToEpi))
+
+format_wksToPeak <- list(w = w, h = h, measure = "wksToPeak", dataProcess = "irDt", legendStep = 4, offset_l = FALSE, dataScale = "reg")
+do.call(choro_obs_db_oneSeason, list(wksToPeak, format_wksToPeak))
+do.call(choro_obs_db_avgSeason, list(wksToPeak, format_wksToPeak))
+
+format_iliEarly <- list(w = w, h = h, measure = "iliEarly", dataProcess = "irDt", legendStep = 250, offset_l = FALSE, dataScale = "reg")
+do.call(choro_obs_db_oneSeason, list(iliEarly, format_iliEarly))
+do.call(choro_obs_db_avgSeason, list(iliEarly, format_iliEarly))
+
+format_iliPeak <- list(w = w, h = h, measure = "iliPeak", dataProcess = "irDt", legendStep = 500, offset_l = FALSE, dataScale = "reg")
+do.call(choro_obs_db_oneSeason, list(iliPeak, format_iliPeak))
+do.call(choro_obs_db_avgSeason, list(iliPeak, format_iliPeak))
