7d32ce866288b4ee9fd1e6106cba7d8ec36c0274
Elizabeth Lee <eclee25@gmail.com>
2017-10-30 14:36:59 -0400

    explore assoc bw variation in db and aggregation bias
    explore_dbVariance_aggBiasMagnitude.R
    - there does not appear to be an association between aggregaton bias
    and variance in disease burden or aggregation bias magnitude and variance
    in disease burden across all 4 measures of disease burden

--- programs/explore_dbVariance_aggBiasMagnitude.R
+++ programs/explore_dbVariance_aggBiasMagnitude.R
@@ -10,7 +10,7 @@ setwd(dirname(sys.frame(1)$ofile))
 setwd("../R_export/test_dbVariance_aggBiasMagnitude")
 ################################
 
-fnames <- list.files()
+fnames <- list.files(pattern = "irDt")
 uqCombos <- gsub(".csv", "", gsub("aggBias_", "", grep("aggBias", fnames, value = TRUE)))
 ctyCombos <- grep("cty", uqCombos, value = TRUE)
 stCombos <- grep("st", uqCombos, value = TRUE)
@@ -20,9 +20,9 @@ import_datasets <- function(combo){
   print(match.call())
 
   biasDat <- read_csv(paste0("aggBias_", combo, ".csv"), col_types = "cdd")
-  varDat <- read_csv(paste0("dbVariance_", combo, ".csv"), col_types = "cdiid")
+  varDat <- read_csv(paste0("dbVariance_", combo, ".csv"))
   scale <- unlist(strsplit(combo, "_"))[1]
-  measure <- unlist(strsplit(combo, "_"))[2]
+  measure <- unlist(strsplit(combo, "_"))[3] # 3 for irDt measures, 2 for non-irDt measures
   print(measure)
   fullDat <- full_join(biasDat, varDat) %>%
     mutate(combo = combo) %>%
@@ -32,9 +32,10 @@ import_datasets <- function(combo){
 }
 
 #### MAIN ################################
+#### statistics ##################################
 ctyDat <- map_df(ctyCombos, import_datasets) # bound dataframes
 stDat <- map_df(stCombos, import_datasets)
-uqMeasures <- gsub("cty_", "", ctyCombos)
+uqMeasures <- gsub("cty_irDt_", "", ctyCombos)
 
 ctyTestResults <- purrr::map(uqMeasures, function(x){
   measureDat <- ctyDat %>% filter(measure == x)
@@ -47,3 +48,30 @@ stTestResults <- purrr::map(uqMeasures, function(x){
   testresults <- cor.test(measureDat$obs_aggBiasMag, measureDat$variance, paired = TRUE)
   return(testresults)
 }) # All measures but wksToEpi are correlated. Magnitude measures have much stronger correlations than timing ones.
+
+#### clean data ##################################
+prep_plotDatSt <- purrr::map(uqMeasures, function(x){
+  measureDat <- stDat %>% 
+    filter(measure == x) %>%
+    filter(!is.na(st))
+  pltLabels <- measureDat %>% arrange(hetRank) %>% select(hetRank, st)
+  measureDat2 <- measureDat %>%
+    mutate(xplot = factor(hetRank, levels = pltLabels$hetRank, labels = pltLabels$st))
+  return(measureDat2)
+})
+
+#### plot data ##################################
+
+plot_st_function <- function(plotDat){
+  plotSt <- ggplot(plotDat, aes(x = xplot, y = obs_aggBiasMag)) +
+    geom_point() + 
+    scale_x_discrete(paste("State, ranked by variance in", plotDat$measure[1])) +
+    scale_y_continuous("Magnitude in Aggregation Bias") +
+    theme_bw() +
+    theme(axis.text =element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, size = 10))
+  return(plotSt)
+} 
+
+stPlots <- lapply(prep_plotDatSt, plot_st_function)
+# There doesn't seem to be a relationship between variance in disease burden and aggregation bias or aggreation bias magnitude
+# 10/30/17
